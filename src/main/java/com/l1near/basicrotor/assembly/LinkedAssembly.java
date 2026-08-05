8128d118d95b07e16ab9221bcbb1fb046f8a7d36
the-real-l1near <minhchaunguyen237@gmail.com>
2026-08-05 20:25:56 +0800

    feat(assembly): implement LinkedAssembly

---
+++ src/main/java/com/l1near/basicrotor/assembly/LinkedAssembly.java
@@ -0,0 +1,81 @@
+package com.l1near.basicrotor.assembly;
+
+/*
+---------------------------
+Imports
+---------------------------
+*/
+
+import net.minecraft.core.BlockPos;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+
+public class LinkedAssembly {
+
+    /*
+    ---------------------------
+    Fields
+    ---------------------------
+    */
+
+    private final BlockPos originPos;
+
+    private final LinkedHashMap<BlockPos, LinkedBlockData> linkedBlocks;
+
+    private AssemblyState state;
+
+    /*
+    ---------------------------
+    Constructors
+    ---------------------------
+    */
+
+    public LinkedAssembly(BlockPos originPos) {
+        this.originPos = originPos;
+        this.linkedBlocks = new LinkedHashMap<>();
+        this.state = AssemblyState.STOPPED;
+    }
+
+    /*
+    ---------------------------
+    Methods
+    ---------------------------
+    */
+
+    //Add
+    public void addBlock(
+            BlockPos relativePos,
+            LinkedBlockData blockData
+    ) {
+        linkedBlocks.put(relativePos, blockData);
+    }
+
+    //Remove
+    public void removeBlock(BlockPos relativePos) {
+        linkedBlocks.remove(relativePos);
+    }
+
+    //Contains
+    public boolean contains(BlockPos relativePos) {
+        return linkedBlocks.containsKey(relativePos);
+    }
+
+    //Getters
+    public BlockPos getOriginPos() {
+        return originPos;
+    }
+
+    public LinkedHashMap<BlockPos, LinkedBlockData> getLinkedBlocks() {
+        return linkedBlocks;
+    }
+    public AssemblyState getState() {
+        return state;
+    }
+
+    //Setters
+    public void setState(AssemblyState state) {
+        this.state = state;
+    }
+
+}
